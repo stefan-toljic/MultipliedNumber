894d89cb23bddaa67ba439e33cd83edbf319470b
stefanToljic <85561921+stefan-toljic@users.noreply.github.com>
2021-06-16 13:40:48 +0000

    Added error handling in the form of a try except block.

--- main.py
+++ main.py
@@ -4,9 +4,18 @@
 Ask user to enter three numbers. If first number is > 10 write sum of second and third number to the console. If first number is <=10 write it directly to the console.
 """
 
-num1 = int(input("Please enter number 1:\n\t"))
-num2 = int(input("Please enter number 2:\n\t"))
-num3 = int(input("Please enter number 3:\n\t"))
+def handle_input(message):
+    while True:
+        try:
+          user_input = int(input(message))
+          break
+        except ValueError:
+            print("Invalid input: Please enter an integer.\n\t")
+    return user_input
+
+num1 = handle_input("Please enter number 1:\n\t")
+num2 = handle_input("Please enter number 2:\n\t")
+num3 = handle_input("Please enter number 3:\n\t")
 
 if num1 > 10:
     print("\n- The sum of the 2nd and 3rd numbers is {}.".format(num2 + num3))
